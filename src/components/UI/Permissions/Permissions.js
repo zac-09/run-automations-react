460112394bd870aef2b8434cc6f0d5550cde1bec
zac <isaacmubiru99@gmail.com>
2021-08-18 17:29:27 +0300

    fix end point

--- src/components/UI/Permissions/Permissions.js
+++ src/components/UI/Permissions/Permissions.js
@@ -1,8 +1,19 @@
 import React from "react";
 import { Fragment } from "react";
-
+const roles = {
+  admin: ["create", "read", "update", "delete"],
+  client: [],
+};
+const actions = ["create", "read", "update", "delete"];
 const Permissions = (props) => {
-  return <Fragment></Fragment>;
+  const { role, action, restrictTo } = props;
+  const displayModule = role === restrictTo ? true : false;
+
+  return (
+    <Fragment>
+      {displayModule && <Fragment> {props.children}</Fragment>}
+    </Fragment>
+  );
 };
 
 export default Permissions;
